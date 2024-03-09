3e2700ab79252e6c481ff8648c4b603d5415be60
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-03-09 21:08:35 +0100

    giffApp acabada

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,28 +1,23 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["One punch", "Dragon Ball"]);
+  const [categories, setCategories] = useState(["One Punch"]);
 
-  const addCategories = () => {
-    setCategories(["Naruto", ...categories]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
   };
 
   return (
     <>
-      {/*Titulo */}
       <h1>GifExpertApp</h1>
-      {/*Input */}
-      <AddCategory />
-      {/*Listado de Gif */}
-      <button onClick={addCategories}>Agregar</button>
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
 
-      {/*Gif items */}
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
--- gif-expert-app/src/components/AddCategory.jsx
+++ gif-expert-app/src/components/AddCategory.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-export const AddCategory = () => {
+export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState("");
 
-  //Desectructuramos el target del objeto event que nos viene por parametros
   const onInputChange = ({ target }) => {
-    //console.log(event.target.value);
     setInputValue(target.value);
   };
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(inputValue);
+    if (inputValue.trim().length <= 1) return;
+
+    // setCategories( categories => [ inputValue, ...categories ]);
+    setInputValue("");
+    onNewCategory(inputValue.trim());
   };
 
   return (
